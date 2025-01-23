dc2aa938e4fb9caff480200ec92160c5bc206ea1
Ivbnt <Ivane74.bonnet@gmail.com>
2025-01-23 23:14:09 +0100

    transitionWork

--- Portfolio/index.js
+++ Portfolio/index.js
@@ -18,12 +18,3 @@ observer.observe(workSection);
 
 
 
-const work2 = document.getElementById('work2');
-const obs = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      work2.classList.add('visible');
-    }
-  });
-}, { threshold: 0.1 });
-obs.observe(work2);
